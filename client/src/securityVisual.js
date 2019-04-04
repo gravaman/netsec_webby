183e57830e52ee5951169b180aa7e4154b27bd30
fleish <cfleish@gmail.com>
2019-04-04 19:35:02 -0400

    adds world map to security visual

--- client/src/securityVisual.js
+++ client/src/securityVisual.js
@@ -1,7 +1,61 @@
 // main security visual
-console.log("main security visual!")
 
+buildMap()
+
+// d3 helpers
+function buildMap() {
+    let svg = d3.select("#security-chart")
+                    .append("svg")
+                        .attr("width", "100%")
+                        .attr("height", "100%")
+                        .attr("viewBox", `0 0 800 500`)
+                        .attr("preserveAspectRatio", "xMinYMin meet")
+
+    pullData().then(function(data) {
+        [worldMap, dnsAll, dnsLocal] = data
+        console.log("worldMap:", worldMap)
+        console.log("dnsAll:", dnsAll)
+        console.log("dnsLocal:", dnsLocal)
+
+        // plot data
+        let projection = d3.geoMercator()
+                            .fitSize([parseFloat(svg.style("width")), parseFloat(svg.style("height"))], worldMap)
+        let worldpath = d3.geoPath(projection)
+
+        svg.selectAll("path")
+                .data(worldMap.features)
+                .enter().append("path")
+                    .attr("class", "world")
+                    .attr("d", worldpath)
+
+    })
+}
+
+function pullData(){
+    let paths = {
+        worldMap: 'world-countries.json',
+        dnsAll: 'dns_records_all.csv',
+        dnsLocal: 'dns_records_local.csv'
+    }
+
+    function pullMap(path) {
+        return d3.json(path)
+    }
+
+    function pullDns(path) {
+        return d3.csv(path, function(record) {
+            return {
+                ip: record.ip,
+                domain: record.domain,
+                malicious: (record.malicious == 'true')
+            }
+        })
+    }
+
+    return Promise.all([pullMap(paths.worldMap), pullDns(paths.dnsAll), pullDns(paths.dnsLocal)])
+}
+
+// HMR
 if (module.hot) {
-    console.log("YES THIS IS A THING")
     module.hot.accept()
 }
