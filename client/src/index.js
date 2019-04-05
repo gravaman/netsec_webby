387a60dbf744b79881f1eaa2fcd563cefeabdc0c
fleish <cfleish@gmail.com>
2019-04-05 01:01:37 -0400

    updates settings sidebar

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,12 @@
 import './securityVisual'
 
+$("#period-buttons").find(".btn").click(function() {
+    $("#period-buttons").find(".btn")
+        .removeClass("btn-success active")
+        .addClass("btn-secondary")
+    $(this).removeClass("btn-secondary")
+            .addClass("btn-success active")
+})
+
 // client js
 console.log("skynet online")
--- client/src/securityVisual.js
+++ client/src/securityVisual.js
@@ -1,5 +1,4 @@
 // main security visual
-
 buildMap()
 
 // d3 helpers
@@ -8,7 +7,7 @@ function buildMap() {
                     .append("svg")
                         .attr("width", "100%")
                         .attr("height", "100%")
-                        .attr("viewBox", `0 0 800 500`)
+                        .attr("viewBox", `0 0 900 500`)
                         .attr("preserveAspectRatio", "xMinYMin meet")
 
     pullData().then(function(data) {
@@ -28,10 +27,6 @@ function buildMap() {
                     .attr("class", "world")
                     .attr("d", worldpath)
 
-        // must be longitude / latitude
-        let london = projection([-0.12574, 51.50853])
-        let nyc = projection([-73.9808, 40.7648])
-
         svg.selectAll("circle")
             .data(dnsAll)
             .enter().append("circle")
@@ -41,21 +36,6 @@ function buildMap() {
                 .attr("cy", d => projection([d.longitude, d.latitude])[1])
                 .attr("stroke", d => d.malicious ? "#FF6347" : "#90EE90")
                 .attr("stroke-width", d => d.malicious ? 5 : 2)
-
-        // svg.append("circle")
-        //     .attr("class", "ipaddr")
-        //     .attr("r", 2)
-        //     .attr("cx", london[0])
-        //     .attr("cy", london[1])
-        //
-        // svg.append("circle")
-        //     .attr("class", "ipaddr")
-        //     .attr("r", 2)
-        //     .attr("cx", nyc[0])
-        //     .attr("cy", nyc[1])
-
-        console.log("projection...")
-        console.log(projection, projection([51.5, 0.13]), worldpath)
     })
 }
 
