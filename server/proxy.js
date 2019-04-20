786e0c0bdc51cff9bee59cfec215f05c2193aaff
fleish <cfleish@gmail.com>
2019-04-20 14:29:09 -0400

    implements classifier request

--- server/proxy.js
+++ server/proxy.js
@@ -4,12 +4,15 @@ const url = require("url")
 const net = require('net')
 const httpProxy = require("http-proxy")
 const geoip = require('geoip-lite')
+const rp = require('request-promise-native')
 const db = require('./db')
 
 const proxyPort = 8080
 const proxySslPort = 8443
 const defaultssl = 443
 const wsPort = 3001
+const clsPort = 5000
+const clsAddr = '0.0.0.0'
 
 let proxy = httpProxy.createProxyServer({})
 proxy.on("error", function (err, req, res) {
@@ -25,47 +28,58 @@ let server = http.createServer(function (req, res) {
   let dnsRecord = {
       domain: urlObj.host,
       ip: null,
-      malicious: isMalicious(urlObj.host),
+      malicious: null,
       last_req: new Date().toUTCString(),
       longitude: null,
       latitude: null,
       frequency: 1
   }
 
-  getIp(urlObj.host)
-      .then(addrs => {
-          if (addrs.length > 0) {
-              dnsRecord.ip = addrs[0]
-          }
-          // get existing db record if exists
-          return db.any('SELECT * FROM requests WHERE domain = $1', urlObj.host)
-      })
-      .then(result => {
-          if (result.length == 0) {
-              let geo = geoip.lookup(dnsRecord.ip)
-              if (geo && geo.ll) {
-                  dnsRecord.latitude = geo.ll[0]
-                  dnsRecord.longitude = geo.ll[1]
-              }
+  rp(`http://${clsAddr}:${clsPort}?url=${urlObj.host}`)
+    .then(function(res) {
+        if (res == 'True') {
+            dnsRecord.malicious = true
+        } else {
+            dnsRecord.malicious = false
+        }
+        return dnsRecord
+    })
+    .then(dnsRecord => {
+        return getIp(urlObj.host)
+    })
+    .then(addrs => {
+        if (addrs.length > 0) {
+            dnsRecord.ip = addrs[0]
+        }
+        // get existing db record if exists
+        return db.any('SELECT * FROM requests WHERE domain = $1', urlObj.host)
+    })
+    .then(result => {
+        if (result.length == 0) {
+            let geo = geoip.lookup(dnsRecord.ip)
+            if (geo && geo.ll) {
+                dnsRecord.latitude = geo.ll[0]
+                dnsRecord.longitude = geo.ll[1]
+            }
 
-              return db.none('INSERT INTO requests (domain, ip, malicious, last_req, longitude, latitude, frequency) \
-                  VALUES (${domain}, ${ip}, ${malicious}, ${last_req}, ${longitude}, ${latitude}, ${frequency})', dnsRecord)
-          } else {
-              return db.none('UPDATE requests SET frequency = $1 WHERE id = $2', [result[0].frequency + 1, result[0].id])
-          }
-      })
-      .then(() => {
-          if (dnsRecord.malicious) {
-              res.writeHead(302, { 'Location': `https://localhost:${ wsPort }` })
-              res.end()
-          } else {
-              proxy.web(req, res, { target: target, prependPath: false })
-          }
-      })
-      .catch(e => {
-          console.error(e)
-          res.end()
-      })
+            return db.none('INSERT INTO requests (domain, ip, malicious, last_req, longitude, latitude, frequency) \
+                VALUES (${domain}, ${ip}, ${malicious}, ${last_req}, ${longitude}, ${latitude}, ${frequency})', dnsRecord)
+        } else {
+            return db.none('UPDATE requests SET frequency = $1 WHERE id = $2', [result[0].frequency + 1, result[0].id])
+        }
+    })
+    .then(() => {
+        if (dnsRecord.malicious) {
+            res.writeHead(302, { 'Location': `https://localhost:${ wsPort }` })
+            res.end()
+        } else {
+            proxy.web(req, res, { target: target, prependPath: false })
+        }
+    })
+    .catch(e => {
+        console.error(e)
+        res.end()
+    })
 }).listen(proxyPort, () => console.log(`listening on ${ proxyPort }`))
 
 server.addListener('connect', function (req, socket, bodyhead) {
@@ -77,7 +91,7 @@ server.addListener('connect', function (req, socket, bodyhead) {
   let dnsRecord = {
       domain: hostDomain,
       ip: null,
-      malicious: isMalicious(hostDomain),
+      malicious: null,
       last_req: new Date().toUTCString(),
       longitude: null,
       latitude: null,
@@ -97,51 +111,52 @@ server.addListener('connect', function (req, socket, bodyhead) {
         .on('end', () => proxySocket.end())
         .on('error', () => proxySocket.end())
 
-  getIp(hostDomain)
-      .then(addrs => {
-          if (addrs.length > 0) {
-              dnsRecord.ip = addrs[0]
-          }
-          // get existing db record if exists
-          return db.any('SELECT * FROM requests WHERE domain = $1', hostDomain)
-      })
-      .then(result => {
-          if (result.length == 0) {
-              let geo = geoip.lookup(dnsRecord.ip)
-              if (geo && geo.ll) {
-                  dnsRecord.latitude = geo.ll[0]
-                  dnsRecord.longitude = geo.ll[1]
-              }
+  rp(`http://${clsAddr}:${clsPort}?url=${hostDomain}`)
+    .then(function(res) {
+        if (res == 'True') {
+            dnsRecord.malicious = true
+        } else {
+            dnsRecord.malicious = false
+        }
 
-              return db.none('INSERT INTO requests (domain, ip, malicious, last_req, longitude, latitude, frequency) \
-                  VALUES (${domain}, ${ip}, ${malicious}, ${last_req}, ${longitude}, ${latitude}, ${frequency})', dnsRecord)
-          } else {
-              return db.none('UPDATE requests SET frequency = $1 WHERE id = $2', [result[0].frequency + 1, result[0].id])
-          }
-      })
-      .then(() => {
-          if (isMalicious(hostDomain)) {
-              port = wsPort
-              hostDomain = 'localhost'
-          }
-
-          proxySocket.connect(port, hostDomain, function () {
-              proxySocket.write(bodyhead);
-              socket.write("HTTP/" + req.httpVersion + " 200 Connection established\r\n\r\n")
-            }
-          )
-      })
-      .catch(e => {
-          console.error(e)
-          socket.end()
-      })
+        getIp(hostDomain)
+            .then(addrs => {
+                if (addrs.length > 0) {
+                    dnsRecord.ip = addrs[0]
+                }
+                // get existing db record if exists
+                return db.any('SELECT * FROM requests WHERE domain = $1', hostDomain)
+            })
+            .then(result => {
+                if (result.length == 0) {
+                    let geo = geoip.lookup(dnsRecord.ip)
+                    if (geo && geo.ll) {
+                        dnsRecord.latitude = geo.ll[0]
+                        dnsRecord.longitude = geo.ll[1]
+                    }
+                    return db.none('INSERT INTO requests (domain, ip, malicious, last_req, longitude, latitude, frequency) \
+                        VALUES (${domain}, ${ip}, ${malicious}, ${last_req}, ${longitude}, ${latitude}, ${frequency})', dnsRecord)
+                } else {
+                    return db.none('UPDATE requests SET frequency = $1 WHERE id = $2', [result[0].frequency + 1, result[0].id])
+                }
+            })
+            .then(() => {
+                if (dnsRecord.malicious) {
+                    port = wsPort
+                    hostDomain = 'localhost'
+                }
+                proxySocket.connect(port, hostDomain, function () {
+                    proxySocket.write(bodyhead);
+                    socket.write("HTTP/" + req.httpVersion + " 200 Connection established\r\n\r\n")
+                  }
+                )
+            })
+            .catch(e => {
+                console.error(e)
+                socket.end()
+            })
+    })
 })
-
-function isMalicious(host) {
-    console.log('checking host:', host)
-    // [TBU by classification engine]
-    return host == 'ardcontracting.com' || host == 'www.hgvoracapital.com'
-}
 
 function getIp(host) {
     return new Promise((resolve, reject) => {
